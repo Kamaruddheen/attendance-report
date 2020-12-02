b6ba00931c83fcf9bd53d159d4d075b4057cdb88
Hariesh <67159530+hk-23@users.noreply.github.com>
2020-12-02 18:29:43 +0530

    added is_hod, is_staff, is_hod_or_self decorator modified base.html for messages(jquery)

--- attendance/user_module/decorators.py
+++ attendance/user_module/decorators.py
@@ -1,1 +1,48 @@
-decorators.py
+from django.shortcuts import redirect
+from django.contrib import messages
+from .models import User
+
+def is_hod(function):
+	def wrap(request,*args,**kwargs):
+		if request.user.is_authenticated:
+			if hasattr(request.user,'staffmodel') and request.user.staffmodel.is_hod:
+				return function(request,*args,**kwargs)
+			else:
+				messages.info(request,'Not Authorized')
+				return redirect('homepage')
+		else:
+			return redirect(redirect('login').url+'?next='+request.path)
+	wrap.__doc__ = function.__doc__
+	wrap.__name__ = function.__name__
+	return wrap
+
+def is_staff(function):
+	def wrap(request,*args,**kwargs):
+		if request.user.is_authenticated:
+			if request.user.user_type == 2:
+				return function(request,*args,**kwargs)
+			else:
+				messages.info(request,'Not Authorized')
+				return redirect('homepage')
+		else:
+			return redirect(redirect('login').url+'?next='+request.path)
+	wrap.__doc__ = function.__doc__
+	wrap.__name__ = function.__name__
+	return wrap
+
+def is_hod_or_self(function):
+	def wrap(request,*args,**kwargs):
+		if request.user.is_authenticated:
+			u = request.user
+			if (hasattr(u,'staffmodel') and u.staffmodel.is_hod) or (hasattr(u,'staffmodel') and u.staffmodel.id == kwargs['id']):
+				return function(request,*args,**kwargs)
+			else:
+				messages.info(request,'Not Authorized')
+				print(kwargs['id'],u.id)
+				print(type(kwargs['id']))
+				return redirect('homepage')
+		else:
+			return redirect(redirect('login').url+'?next='+request.path)
+	wrap.__doc__ = function.__doc__
+	wrap.__name__ = function.__name__
+	return wrap
