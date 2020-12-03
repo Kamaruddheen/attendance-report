19cc95688a52a6a6c2775f9dde19faae765d9c06
harishsoccer7 <harishsoccer7@gmail.com>
2020-12-04 00:27:30 +0530

    classroom  required things added
    forms.py : id added in each form field
    when create class button is clicked,fields values are removed

--- attendance/classroom/forms.py
+++ attendance/classroom/forms.py
@@ -4,10 +4,10 @@ from .models import ClassroomModel
 class ClassroomForm(forms.ModelForm):
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
-        self.fields['course'].widget.attrs.update({'class':'form-control'})
-        self.fields['year'].widget.attrs.update({'class':'form-control'})
-        self.fields['sec'].widget.attrs.update({'class':'form-control'})
-        self.fields['tutor'].widget.attrs.update({'class':'form-control'})
+        self.fields['course'].widget.attrs.update({'class':'form-control identify','id':'course'})
+        self.fields['year'].widget.attrs.update({'class':'form-control identify','id':'year'})
+        self.fields['sec'].widget.attrs.update({'class':'form-control identify','id':'section'})
+        self.fields['tutor'].widget.attrs.update({'class':'form-control identify','id':'tutor'})
     class Meta:
         model=ClassroomModel
         fields="__all__"
