04a3a041dc0f7278a4fe92e71b76a3e1f7631227
sujithsingh235 <62689265+sujithsingh235@users.noreply.github.com>
2020-12-07 20:03:15 +0530

    attendance table is changed.
    attendanceModel is changed to AttendanceIdModel.
    new Attendance model is added.
    The subject field in attendanceModel is changed to Foreignkey field.

--- attendance/attendancess/admin.py
+++ attendance/attendancess/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import AttendanceModel
+from .models import AttendanceIdModel, AttendanceModel
 
 
-class AttendanceModelAdmin(admin.ModelAdmin):
+class AttendanceIdModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'date', 'subject', 'status', 'handled_by']
     search_fields = ['date', 'subject', 'handled_by']
 
+class AttendanceModelAdmin(admin.ModelAdmin):
+    list_display = ['attendance_id', 'rollno', 'status']
 
+
+admin.site.register(AttendanceIdModel, AttendanceIdModelAdmin)
 admin.site.register(AttendanceModel, AttendanceModelAdmin)
--- attendance/attendancess/models.py
+++ attendance/attendancess/models.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 from django.db import models
+from subject.models import SubjectModel
 
 
-class AttendanceModel(models.Model):
+class AttendanceIdModel(models.Model):
     date = models.DateField()
-    subject = models.CharField(max_length=50)
+    subject = models.ForeignKey(SubjectModel, on_delete=models.CASCADE)
     handled_by = models.CharField(max_length=150)  # username max length
     status_choice = (('reject', 'Reject'),
                      ('cancel', 'Cancel'), ('calloff', 'Call Off'))
@@ -15,6 +16,17 @@ class AttendanceModel(models.Model):
         related_name="student_attendance")
 
     class Meta:
+        db_table = 'attendanceID'
+        verbose_name = 'AttendanceID'
+        verbose_name_plural = 'AttendancesIDs'
+
+class AttendanceModel(models.Model):
+    attendance_id = models.PositiveIntegerField()
+    rollno = models.CharField(max_length=10)
+    status = models.CharField(max_length=10)  # Present/Absent
+
+    class Meta:
         db_table = 'attendance'
         verbose_name = 'Attendance'
         verbose_name_plural = 'Attendances'
+
