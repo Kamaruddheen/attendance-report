8b24a064b1d42c0a9b66ebce7c30513751306df0
Kamaruddheen <57263951+Kamaruddheen@users.noreply.github.com>
2020-12-09 00:21:18 +0530

    Updated User Login UI & Removed user_module app_name in urls.py
    Changed Ui for Login, Password Reset, Change Password.
    Added border for some cards.

--- attendance/user_module/urls.py
+++ attendance/user_module/urls.py
@@ -3,8 +3,6 @@ from django.contrib.auth import views as auth_views
 
 from .views import *
 
-app_name = 'user_module'
-
 urlpatterns = [
 	path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True),name="login"),
 	path('logout/', auth_views.LogoutView.as_view(), name='logout'),
@@ -14,7 +12,7 @@ urlpatterns = [
 	path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
 	path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 	path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-	#View url patters
+	# View url patters
 	path('create/staff/',createstaffview,name='create_staff'),
 	path('manage/staff/',managestaffview,name='manage_staff'),
 	path('manage/staff/<int:id>',staffdetailview,name='staff_detail'),
