9f86960b60bca9d650e76ec590c4f2146349d868
Kamaruddheen <57263951+Kamaruddheen@users.noreply.github.com>
2020-12-11 18:15:30 +0530

    UI for classroom create modal & few changes
    Changes :
      updated classroom views as formatted.
      edit staff modal can be viewed automatically when error occurs and while cancelling page will reload.

--- attendance/classroom/forms.py
+++ attendance/classroom/forms.py
@@ -2,16 +2,21 @@ from django import forms
 from .models import ClassroomModel
 
 class ClassroomForm(forms.ModelForm):
-    id=forms.CharField(max_length=10,required=False)
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
-        self.fields['id'].widget.attrs.update({'class':'form-control','id':'id','readonly':True})
-        self.fields['course'].widget.attrs.update({'class':'form-control identify','id':'course'})
-        self.fields['year'].widget.attrs.update({'class':'form-control identify'})
-        self.fields['sec'].widget.attrs.update({'class':'form-control identify','id':'section'})
-        self.fields['tutor'].widget.attrs.update({'class':'form-control identify','id':'tutor'})
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['course'].widget.attrs.update(
+            {'class': 'input_cust'})
+        self.fields['year'].widget.attrs.update(
+            {'class': 'input_cust'})
+        self.fields['sec'].widget.attrs.update(
+            {'class': 'input_cust'})
+        self.fields['tutor'].widget.attrs.update(
+            {'class': 'input_cust'})
+        self.fields['course'].label = "Name of the Course"
+        self.fields['tutor'].label = "Tutor Name"
+        self.fields['sec'].required = True
+
     class Meta:
-        model=ClassroomModel
-        fields="__all__"
-
-
+        model = ClassroomModel
+        fields = ['course', 'year', 'sec', 'tutor']
--- attendance/classroom/views.py
+++ attendance/classroom/views.py
@@ -5,25 +5,37 @@ from .models import ClassroomModel
 
 
 def CreateClass(request):
-    if request.method=="POST":
-        ClassroomObject=ClassroomForm(request.POST)
+    if request.method == "POST":
+        ClassroomObject = ClassroomForm(request.POST)
         if ClassroomObject.is_valid():
             ClassroomObject.save()
-            all_classrooms=ClassroomModel.objects.all()
-            messages.success(request,"Classroom created")
-            return render(request,'classroom/view_and_create_classroom.html',{'all_classrooms':all_classrooms})
-        all_classrooms=ClassroomModel.objects.all()
-        errors="having"
-        return render(request,'classroom/view_and_create_classroom.html',{'all_classrooms':all_classrooms,'form':ClassroomObject,'errors':errors})
+            all_classrooms = ClassroomModel.objects.all()
+            messages.success(request, "Classroom created")
+            return render(request, 'classroom/view_and_create_classroom.html', {'all_classrooms': all_classrooms})
+        all_classrooms = ClassroomModel.objects.all()
+        context = {
+            'all_classrooms': all_classrooms,
+            'form': ClassroomObject
+        }
+        return render(request, 'classroom/view_and_create_classroom.html', context=context)
     else:
-        all_classrooms=ClassroomModel.objects.all()
-        ClassroomObject=ClassroomForm()
-        return render(request,'classroom/view_and_create_classroom.html',{'form':ClassroomObject,'all_classrooms':all_classrooms})
+        all_classrooms = ClassroomModel.objects.all()
+        ClassroomObject = ClassroomForm()
+        context = {
+            'form': ClassroomObject,
+            'all_classrooms': all_classrooms
+        }
+        return render(request, 'classroom/view_and_create_classroom.html', context=context)
 
-def editclass(request,id):
-    get_obj=get_object_or_404(ClassroomModel,id=id)
-    editclass=ClassroomForm(request.POST or None,instance=get_obj)
-    if request.method=='POST':
+
+def editclass(request, id):
+    get_obj = get_object_or_404(ClassroomModel, id=id)
+    editclass = ClassroomForm(request.POST or None, instance=get_obj)
+    if request.method == 'POST':
         if editclass.is_valid():
             editclass.save()
-    return render(request,'classroom/edit_classroom.html',{'get_obj':get_obj,'editclass':editclass})
+    context = {
+        'get_obj': get_obj,
+        'editclass': editclass
+    }
+    return render(request, 'classroom/edit_classroom.html', context=context)
