359cdbb38c40fdeb40ebe434f034907ae5f49c5f
Kamaruddheen <57263951+Kamaruddheen@users.noreply.github.com>
2020-12-14 00:12:38 +0530

    UI update for classroom
    Update & Changes includes :
    1) selecting all the classroom to delete
    2) drag & select multiple classroom to delete
    3) card ui updated with hover effect which will show options
    4) new classroom & book image added & updated
    5) classroom jquery is structured & optimized with function
    6)  if the user select any card with drag feature i will be highlighted.

--- attendance/classroom/views.py
+++ attendance/classroom/views.py
@@ -1,3 +1,5 @@
+import os
+from django.conf import settings
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from .forms import ClassroomForm
@@ -21,9 +23,23 @@ def CreateClass(request):
     else:
         all_classrooms = ClassroomModel.objects.all()
         ClassroomObject = ClassroomForm()
+        # image
+        files = os.listdir(os.path.join(
+            settings.STATIC_DIR, "static\\img\\classrooms\\"))
+        mylist = zip(all_classrooms, files)
+
+        # if no. of subjects is greater than the no. of images
+        if len(all_classrooms) > len(files):
+            value = None
+            value = len(all_classrooms) - len(files)
+            for i in range(0, value):
+                files.append("classroom11.jpg")
+            # if no. of images is greater than the no. of subjects
+        elif len(files) > len(all_classrooms):
+            pass
         context = {
             'form': ClassroomObject,
-            'all_classrooms': all_classrooms
+            'mylist': mylist
         }
         return render(request, 'classroom/view_and_create_classroom.html', context=context)
 
