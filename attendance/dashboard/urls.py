e164acc1802ad6ddca7039026119171d8cd295b3
Kamaruddheen <57263951+Kamaruddheen@users.noreply.github.com>
2021-04-11 18:55:29 +0530

    Code optimized in Dashboard section
    + Changed Doughnut chart to ajax request.
    + ajax request for Total (Student, Staff, Present, Absent)
    + Added comments for readability

--- attendance/dashboard/urls.py
+++ attendance/dashboard/urls.py
@@ -5,6 +5,7 @@ app_name = 'dashboard'
 
 urlpatterns = [
     path('', dashboard_view, name="view"),
+    path('all/', all_data, name='all_data'),
     path('student/', student_details, name="student_roll_no"),
     path('attendance_classwise/', attendance_classwise,
          name='attendance_classwise'),
--- attendance/dashboard/views.py
+++ attendance/dashboard/views.py
@@ -7,7 +7,58 @@ from classroom.models import *
 
 
 def dashboard_view(request):
-    data = []
+    classes = []
+
+    # collecting class list
+    list_of_classrooms = AttendanceIdModel.objects.values(
+        'classroom').distinct()
+
+    for clas in list_of_classrooms:
+        cla = ClassroomModel.objects.filter(id=clas['classroom'])
+        for c in cla:
+            # storing name of class
+            classes.append(c)
+
+    context = {
+        'classes': classes
+    }
+    return render(request, "dashboard/dashboard.html", context=context)
+
+
+# Class-wise Attendance stacked-bar chart
+def attendance_classwise(request):
+    classes = []
+    present = []
+    absent = []
+
+    # collecting class list
+    list_of_classrooms = AttendanceIdModel.objects.values(
+        'classroom').distinct()
+
+    for clas in list_of_classrooms:
+        cla = ClassroomModel.objects.filter(id=clas['classroom'])
+        for c in cla:
+            # storing name of class
+            classes.append(str(c).upper())
+        class_present = AttendanceModel.objects.filter(
+            attendance_id__classroom=clas['classroom'], status="Present").count()
+        class_absent = AttendanceModel.objects.filter(
+            attendance_id__classroom=clas['classroom'], status="Absent").count()
+        # storing total present of particular class
+        present.append(class_present)
+        # storing total absent of particular class
+        absent.append(class_absent)
+
+    return JsonResponse({
+        'labels': classes,
+        'present': present,
+        'absent': absent,
+    })
+
+
+# Overall Percentage
+def over_per():
+    overall_percentage = []
     Present_count = 0
     Absent_count = 0
 
@@ -20,53 +71,44 @@ def dashboard_view(request):
     Absent_per = "{:.2f}".format((Absent_count / total) * 100)
 
     # passing presentage data for doughnut chart
-    data = [Present_per, Absent_per]
+    overall_percentage = [Present_per, Absent_per]
 
+    return overall_percentage
+
+
+# Overall Totals
+def over_tot():
     # Total number of students
     total_stud_count = AttendanceModel.objects.values(
         'rollno').distinct().count()
+    # Total number of staff
     staff_count = User.objects.filter(user_type=2).count()
+    # Total Present & Absent Status
+    Present_count = AttendanceModel.objects.filter(status="Present").count()
+    Absent_count = AttendanceModel.objects.filter(status="Absent").count()
 
-    context = {
-        'data': data,
+    return total_stud_count, staff_count, Present_count, Absent_count
+
+
+# Class-wise all type of data
+def all_data(request):
+    # * Default
+    # Percentage of All Data
+    overall_percentage = over_per()
+    # Student, Staff, Present, Absent
+    total_stud_count, staff_count, Present_count, Absent_count = over_tot()
+
+    return JsonResponse({
         'total': total_stud_count,
         'staff': staff_count,
         'present': Present_count,
         'absent': Absent_count,
-    }
-    return render(request, "dashboard/dashboard.html", context=context)
-
-
-def attendance_classwise(request):
-    classes = []
-    present = []
-    absent = []
-
-    list_of_classrooms = AttendanceIdModel.objects.values(
-        'classroom').distinct()
-    print(list_of_classrooms)
-    for clas in list_of_classrooms:
-        cla = ClassroomModel.objects.filter(id=clas['classroom'])
-        for c in cla:
-            classes.append(str(c).upper())
-        class_present = AttendanceModel.objects.filter(
-            attendance_id__classroom=clas['classroom'], status="Present").count()
-        class_absent = AttendanceModel.objects.filter(
-            attendance_id__classroom=clas['classroom'], status="Absent").count()
-        present.append(class_present)
-        absent.append(class_absent)
-
-    print(classes)
-    print(present, absent)
-
-    return JsonResponse({
-        'labels': classes,
-        'present': present,
-        'absent': absent,
+        'overall_perc': overall_percentage
     })
 
-
 # Particular student data
+
+
 def student_details(request):
     rollno = request.POST.get('rollno', None)
     # print(rollno)
